9b6b412132242837efa4355661432f8ff3906f66
Briscoe Kerferd <bjkerferd@pm.me>
2025-04-03 06:52:58 +1030

    Initialise files and implement basic operands in js

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b);
+}
+
+function divide (a, b) {
+    return b !== 0 ? a / b : "DivZero!"; // dive by zero error
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function 
